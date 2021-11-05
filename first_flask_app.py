48bc5f40b52fe7957c0553fb5f674c2ee459b90f
Memo <hyperhorus@gmail.com>
2021-11-04 21:46:39 -0600

    59 Creating our application endpoint

--- first_flask_app.py
+++ first_flask_app.py
@@ -4,8 +4,42 @@ from flask import Flask
 
 app = Flask(__name__)
 
-x@app.route('/')
-def home():
-    return 'Hello world! Welcome to the jungle'
+stores = [
+    {
+        'name':'My wonderful store',
+        'items': [
+            {
+                'name':'my item',
+                'prices': 15.99
+            }
+        ]
+    }
+]
+
+#POST /store data: {name:}
+#Esto crea un endpoint...si un endpoin pero debe ser post, porque debemos crear la store
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+#GET /store/<string:name>
+@app.route('/store/<string:name>') #<string:name> este name debe ser igual al parametro get_store(name):
+def get_store(name): #http://127.0.0.1:5000/name --> este es el 'name'
+    pass
+
+#GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
+#POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST']) #<string:name> este name debe ser igual al parametro get_store(name):
+def create_item_in_store(name): #http://127.0.0.1:5000/name --> este es el 'name'
+    pass
+
+#GET /store/<string:name>/item
+@app.route('/store/<string:name>/item') #<string:name> este name debe ser igual al parametro get_store(name):
+def get_item_in_store(name): #http://127.0.0.1:5000/name --> este es el 'name'
+    pass
 
 #app.run(port=5000)
