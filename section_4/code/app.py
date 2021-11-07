8e0cd75fd6d0eca8d67acefdceb42a184b1d35b9
Memo <hyperhorus@gmail.com>
2021-11-07 11:57:28 -0600

    68 Creating our Item Resource

--- section_4/code/app.py
+++ section_4/code/app.py
@@ -4,8 +4,21 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app) #facilita la creación de resoruces
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
+items = []
 
-api.add_resource(Student, '/student/<string:name>') #http://127.0.0.1:5000/student7Rolf
+
+
+class Item(Resource):
+    def get(self, name):
+        for it in items:
+            if it['name'] == name:
+                return it
+        return {'item': None}, 404  #este numero hace que el codigo de error sea el 404, de Not Found
+
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201   #indica que el item fue creado
+
+api.add_resource(Item, '/item/<string:name>')
