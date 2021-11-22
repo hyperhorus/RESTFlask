eb6f5595762c91e39859966e70ad87e3b43cbb79
Memo <hyperhorus@gmail.com>
2021-11-21 21:09:15 -0600

    83 Retrieving our item resources from a data base

--- section_5/code/create_tables.py
+++ section_5/code/create_tables.py
@@ -7,7 +7,10 @@ cursor = connection.cursor()
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
+create_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cursor.execute(create_table)
 
+cursor.execute("INSERT INTO items VALUES ('test', 10.99)")
 connection.commit()
 
 query = "SELECT * FROM sqlite_master WHERE type='table'"
